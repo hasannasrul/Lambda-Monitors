0eaaf865d29f3c2bd9d39de083038c4173b56748
hasannasrul <hasanali242424@gmail.com>
2023-11-19 15:45:36 +0530

    Refactor

--- main.py
+++ main.py
@@ -1,35 +1,47 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
+from selenium.common.exceptions import TimeoutException
 from tempfile import mkdtemp
-import requests
+from concurrent.futures import ThreadPoolExecutor, as_completed
 
-def get_page_load_time(driver):
-    navigation_start = driver.execute_script("return window.performance.timing.navigationStart;")
-    load_event_end = driver.execute_script("return window.performance.timing.loadEventEnd;")
-    page_load_time = (load_event_end - navigation_start) / 1000.0  # Convert to seconds
-    return page_load_time
 
-def check_console_errors(driver):
-    console_errors = driver.get_log("browser")
-    return console_errors
+class Monitor():
+    def __init__(self, driver):
+        self.driver = driver
 
-# def check_external_links(driver):
-#     links = driver.find_elements(by=By.TAG_NAME, value="a")
-#     external_links = [link.get_attribute('href') for link in links if link.get_attribute('href') and 'example.com' not in link.get_attribute('href')]
+    def get_page_load_time(self):
+        navigation_start = self.driver.execute_script("return window.performance.timing.navigationStart;")
+        load_event_end = self.driver.execute_script("return window.performance.timing.loadEventEnd;")
+        page_load_time = (load_event_end - navigation_start) / 1000.0  # Convert to seconds
+        return page_load_time
 
-#     broken_links = []
+    def check_console_errors(self):
+        console_errors = self.driver.get_log("browser")
+        return console_errors
 
-#     for link in external_links:
-#         try:
-#             response = requests.head(link, allow_redirects=True, timeout=5)
-#             if response.status_code != 200:
-#                 broken_links.append(link)
-#         except Exception as e:
-#             print(f"Error checking link {link}: {e}")
-#             broken_links.append(link)
+    def check_url(self, url, timeout=10):
+        try:
+            self.driver.set_page_load_timeout(timeout)
+            self.driver.get(url)
+            # Check the HTTP status code
+            status_code = self.driver.execute_script("return window.performance.getEntries()[0].response.status")
+            if status_code == 200 or status_code == 403:
+                return None  # Website is up
+            else:
+                return { url : status_code }
+        except TimeoutException:
+            return { url : "Timeout" }  # Timeout occurred, URL might be down or slow to respond
+        except Exception as e:
+            return { url : str(e) }  # Other exceptions, URL is likely broken
 
-#     return broken_links
+    def check_urls(self, urls, timeout=10):
+        with ThreadPoolExecutor() as executor:
+            # Create a list of futures for each URL
+            futures = [executor.submit(self.check_url, url, timeout) for url in urls]
+            # Collect broken URLs
+            broken_urls = [future.result() for future in as_completed(futures) if future.result() is not None]
+            return broken_urls
 
 def handler(event, context):
     # Extract URL from the event
@@ -65,32 +77,34 @@ def handler(event, context):
 
         driver = webdriver.Chrome(options=options, service=service)
 
-        # Open the provided URL in the browser
-        driver.get(url)
-        page_load_time = get_page_load_time(driver)
+        # create a monitor class instance
+        monitor = Monitor(driver)
 
-        # Fetch all links on the page
+        # 1. Open the provided URL in the browser
+        driver.get(url)
+
+        # 2. Check Page Load Time
+        page_load_time = monitor.get_page_load_time()
+        
+        # 3. Check for console errors
+        console_errors = monitor.check_console_errors()
+
+        # 4. Fetch all links on the page to Check external links for being broken
         links = driver.find_elements(by=By.TAG_NAME, value="a")
         link_texts = [link.get_attribute('href') for link in links if link.get_attribute('href').strip()]
-        
-        # Check for console errors
-        console_errors = check_console_errors(driver)
 
-        # # Check external links for being broken
-        # broken_links = check_external_links(driver)
+        broken_links = monitor.check_urls(link_texts)
 
         return {
             'site-available': True,
             'page-load-time': page_load_time,
             'console-errors': console_errors,
-            'body': {
-                'links-texts': link_texts,
-                # 'broken-links': broken_links
-            }
+            'broken-links': broken_links,
+            'links-texts': link_texts
+            
         }
 
     finally:
         # Close the browser in the finally block to ensure cleanup
         if driver:
             driver.quit()
-
